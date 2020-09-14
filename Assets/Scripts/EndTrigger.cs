146e138791793c69d05977bebc7d52a4757903ef
Andrew Martin <mochi.yuki64@gmail.com>
2020-09-13 21:09:02 -0700

    Add level complete object

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    void OnTriggerEnter(Collider target)
+    {
+        if(target.tag == "Player")
+        {
+            gameManager.CompleteLevel();
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    public GameObject pickupEffect;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Player")){
+            Pickup();
+        }
+    }
+
+    private void Pickup()
+    {
+        //Debug.Log("PowerUp Picked Up");
+        
+        //Spawn a cool effect
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //Apply effect to player
+
+        //Remove power up
+        Destroy(gameObject);
+    }
+}
