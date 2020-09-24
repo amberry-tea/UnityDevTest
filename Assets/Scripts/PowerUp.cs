3ab3110ad0abf8e11a167f3d54aab7507fad6058
Andrew Martin <mochi.yuki64@gmail.com>
2020-09-23 21:46:20 -0700

    Add power up object. Add player stats class.

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -6,22 +6,64 @@ public class PowerUp : MonoBehaviour
 {
 
     public GameObject pickupEffect;
+    public float duration = 4f;
+    //public int healthBoost = 20;
 
     private void OnTriggerEnter(Collider other) 
     {
         if(other.CompareTag("Player")){
-            Pickup();
+            StartCoroutine(Pickup(other));
         }
     }
 
-    private void Pickup()
+    private IEnumerator Pickup(Collider player)
     {
         //Debug.Log("PowerUp Picked Up");
         
         //Spawn a cool effect
         Instantiate(pickupEffect, transform.position, transform.rotation);
-
+        //~~~~~~~~~~~~~~~~~~~~~~
         //Apply effect to player
+        //~~~~~~~~~~~~~~~~~~~~~~
+
+        player.transform.localScale *= 1.1f; //Make the player bigger by 50%
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale /= 1.1f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale *= 1.2f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale /= 1.2f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale *= 1.3f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale /= 1.3f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale *= 1.5f;
+
+        //PlayerStats stats = player.GetComponent<PlayerStats>();
+        //stats.health += healthBoost;
+
+        //Make the powerup invisible as it has been collected
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+        //Wait a time
+        yield return new WaitForSeconds(duration);
+
+        //Reverse effect on player
+        player.transform.localScale /= 1.2f; //Make the player smaller by 50%
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale *= 1.2f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale /= 1.3f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale *= 1.3f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale /= 1.4f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale *= 1.4f;
+        yield return new WaitForSeconds(.1f);
+        player.transform.localScale /= 1.5f;
 
         //Remove power up
         Destroy(gameObject);
