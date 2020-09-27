a631fc9143f5c8cc9d1cfbabc721c7846fb68c7f
Andrew Martin <mochi.yuki64@gmail.com>
2020-09-27 11:27:11 -0700

    Add enemy

---
+++ Assets/_TopDownShooter/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TopDownShooter
+{
+    [RequireComponent (typeof(NavMeshAgent))]
+    public class Enemy : MonoBehaviour
+    {
+        NavMeshAgent pathfinder;
+        Transform target;
+
+        void Start()
+        {
+            pathfinder = GetComponent<NavMeshAgent>();
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+
+            StartCoroutine(UpdatePath());
+        }
+
+        void Update()
+        {
+        }
+        IEnumerator UpdatePath() {
+            float refreshRate = .25f;
+
+            while(target != null){
+                Vector3 targetPos = new Vector3(target.position.x, 0, target.position.z);
+                pathfinder.SetDestination (targetPos);
+                yield return new WaitForSeconds(refreshRate);
+            }
+        }
+    }
+}
--- Assets/_TopDownShooter/Scripts/Projectile.cs
+++ Assets/_TopDownShooter/Scripts/Projectile.cs
@@ -8,6 +8,7 @@ namespace TopDownShooter
     {
 
         float speed = 10;
+        public LayerMask collisionMask; //Determine which layers the projectile can collide with
 
         public void SetSpeed(float newSpeed)
         {
@@ -16,7 +17,32 @@ namespace TopDownShooter
 
         void Update()
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            float moveDistance = speed * Time.deltaTime;
+
+            CheckCollisions(moveDistance);
+
+            transform.Translate(Vector3.forward * moveDistance);
+        }
+
+        /**
+        * Check if the bullet hits anything
+        */
+        void CheckCollisions(float moveDistance)
+        {
+            Ray ray = new Ray(transform.position, transform.forward); //A ray infront of the bullet to calculate if it will hit anything
+            RaycastHit hit;
+
+            //QueryTriggerInteraction determines if the raycast should also check trigger objects
+            //We're checking for triggers since we've set the enemies to triggers
+            if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+            {
+                OnHitObject(hit);
+            }
+        }
+
+        void OnHitObject(RaycastHit hit){
+            print(hit.collider.gameObject.name);
+            GameObject.Destroy(gameObject);
         }
     }
 }
